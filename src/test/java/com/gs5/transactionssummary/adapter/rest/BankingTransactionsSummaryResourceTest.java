82524cb62f91e95f8f6ea4dd7b12954c36f7b42d
Grzech <grzech4456@gmail.com>
2025-04-28 12:26:58 +0200

    add e2e tests

--- src/test/java/com/gs5/transactionssummary/adapter/rest/BankingTransactionsSummaryResourceTest.java
+++ src/test/java/com/gs5/transactionssummary/adapter/rest/BankingTransactionsSummaryResourceTest.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
 import io.restassured.response.ResponseBodyExtractionOptions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -27,6 +30,7 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.OK;
 
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT, properties = "spring.main.allow-bean-definition-overriding=true")
 class BankingTransactionsSummaryResourceTest {
 
@@ -38,8 +42,34 @@ class BankingTransactionsSummaryResourceTest {
         RestAssured.port = port;
     }
 
+    @Order(1)
     @ParameterizedTest
-    @MethodSource("testCases")
+    @MethodSource("getAccountsTestCases")
+    public void shouldGetAccounts(String response, HttpStatus httpStatus) throws IOException {
+        final var responseExcepted = getJson(response);
+
+        ResponseBodyExtractionOptions body = RestAssured.with()
+                .header("Content-Type", "application/json")
+                .when()
+                .get("/bankingtransactions/accounts")
+                .then()
+                .assertThat()
+                .statusCode(httpStatus.value())
+                .extract()
+                .body();
+
+        assertJsonEquals(responseExcepted, body);
+    }
+
+    private static Stream<Arguments> getAccountsTestCases() {
+        return Stream.of(
+                of("get_accounts_success_response.json", OK)
+        );
+    }
+
+    @Order(2)
+    @ParameterizedTest
+    @MethodSource("summaryTestCases")
     public void shouldGenerateSummary(String contractJson, String summaryJson, HttpStatus httpStatus) throws IOException {
         final var contract = getJson(contractJson);
         final var summary = getJson(summaryJson);
@@ -58,7 +88,7 @@ class BankingTransactionsSummaryResourceTest {
         assertJsonEquals(summary, body);
     }
 
-    private static Stream<Arguments> testCases() {
+    private static Stream<Arguments> summaryTestCases() {
         return Stream.of(
                 of("transactions_with_same_currencies_request_successful.json", "transactions_with_same_currencies_response_successful.json", OK),
                 of("transactions_with_different_currencies_request_successful.json", "transactions_with_same_currencies_response_successful.json", OK),
@@ -78,6 +108,33 @@ class BankingTransactionsSummaryResourceTest {
     private void assertJsonEquals(String summary, ResponseBodyExtractionOptions body) throws IOException {
         final ObjectMapper mapper = new ObjectMapper();
         assertEquals(mapper.readTree(Objects.requireNonNull(summary)), mapper.readTree(body.asByteArray()));
+    }
+
+    @Order(3)
+    @ParameterizedTest
+    @MethodSource("upgradeAccountBalanceAndDateTestCases")
+    public void shouldUpgradeAccountBalanceAndDate(String requestJson, String responseJson, HttpStatus httpStatus) throws IOException {
+        final var request = getJson(requestJson);
+        final var response = getJson(responseJson);
+
+        ResponseBodyExtractionOptions body = RestAssured.with()
+                .body(request)
+                .header("Content-Type", "application/json")
+                .when()
+                .post("/bankingtransactions/create-or-update-accounts")
+                .then()
+                .assertThat()
+                .statusCode(httpStatus.value())
+                .extract()
+                .body();
+
+        assertJsonEquals(response, body);
+    }
+
+    private static Stream<Arguments> upgradeAccountBalanceAndDateTestCases() {
+        return Stream.of(
+                of("upgrade_account_balance_and_date_request_successful.json", "upgrade_account_balance_and_date_response_successful.json", OK)
+        );
     }
 
 
